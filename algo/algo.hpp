e730ab3b520b5b060feaf6b3ed4b6a32c46da8d4
xzt <xzt@ubuntu.(none)>
2013-08-06 22:26:59 -0400

    misc fixes

---
+++ algo/algo.hpp
@@ -0,0 +1,16 @@
+#ifndef ALGO_H
+#define ALGO_H
+
+bool isPrime(const int &num);
+
+bool isPalindromeStr(const std::string& str);
+
+
+/* Test APIs */
+// Prime number
+void TEST_isPrime();
+
+// Palindrome String
+void TEST_isPalindromeStr(); 
+
+#endif
--- algo/prime.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PRIME_H
-#define PRIME_H
-
-bool isPrime(const int &num);
-void TEST_isPrime();
-
-#endif
