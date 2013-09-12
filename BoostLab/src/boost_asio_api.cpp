31f065fa1bb94fdad05841813c0c8c3bf5771916
goofycoder <yazhou2@linux01.(none)>
2013-09-12 15:15:44 -0400

    add io_service.reset() example

--- BoostLab/src/boost_asio_api.cpp
+++ BoostLab/src/boost_asio_api.cpp
@@ -3,18 +3,29 @@
     boost::asio::io_service::work work(io_service);     // 'work' class is a "class to inform io_service when it has work to do." 
                                                         // As long as an io_service has a work object associated with it, it will never run out of stuff to do. 
     
-    io_service.run();   // run io_service object's event processing loop
+    io_service.run()    // run io_service object's event processing loop
                         // run() blocks until all work has finished and there are no more handlers to be dispatched,
                         //       or until the io_service has been stopped
+                        // Multiple threads may call the run() function to set up a pool of threads 
+                        // from which the io_service may execute handlers. 
+                        // All threads that are waiting in the pool are equivalent and 
+                        // the io_service may choose any one of them to invoke a handler.
 
-
-
+    io_service.poll()   // Run io_service object's event processing loop to execute ready handlers.
+                        // poll() function runs handlers that are ready to run, ** without blocking**
+                        //        until the io_service has been stopped or there are no more ready handlers.
+ 
+    io_service.reset()  // Remove a work object from io_service
+                        // must be called prior to any second or later set of invocations of the run(), run_one(), poll() or poll_one() functions 
+                        // when a previous invocation of these functions returned due to the io_service being stopped or running out of work.
  */
 #include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
 #include <iostream>
 
 static void example_run();
 static void example_poll();
+static void example_reset();
 
 static void example_run()
 {
@@ -23,7 +34,7 @@ static void example_run()
     
     io_service.run();  
 
-    std::cout << "Event processing finishes" << std::endl;      // never reach here if io_server has work object associated with
+    std::cout << "Event processing is not blocked." << std::endl;      // never reach here if io_server has work object associated with
 }
 
 static void example_poll()
@@ -34,9 +45,22 @@ static void example_poll()
     boost::asio::io_service::work work(io_service);
 
     for (int i=0; i<NUM_TASKS; i++) {
-        io_service.poll();
-        std::cout << "Task #" << i << "\n";
+        std::size_t rv;
+        rv = io_service.poll();
+        std::cout << "Task #" << i << " with return value: " << rv << "\n";
     }
+}
+
+// remove a work object from an io_service
+static void example_reset()
+{
+    boost::asio::io_service io_service;
+    boost::shared_ptr<boost::asio::io_service::work> work(new boost::asio::io_service::work(io_service));
+
+    work.reset();
+    io_service.run();
+    
+    std::cout << "Event processing is not blocked." << std::endl;     
 }
 
 void TEST_boost_asio_api()
@@ -46,4 +70,7 @@ void TEST_boost_asio_api()
 
     std::cout << "\nExample: io_service poll:\n";
     example_poll();
+    
+    std::cout << "\nExample: io_service reset:\n";
+    example_reset();
 }
