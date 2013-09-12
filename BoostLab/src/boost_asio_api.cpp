8470a8385ee3954fde4b77c2842a355fe6d26e9c
goofycoder <yazhou2@linux01.(none)>
2013-09-12 14:56:56 -0400

    add example for io_service.poll()

--- BoostLab/src/boost_asio_api.cpp
+++ BoostLab/src/boost_asio_api.cpp
@@ -1,16 +1,49 @@
-#include <boost/asio.hpp>
-#include <iostream>
-
-void TEST_boost_asio_api()
-{
-    boost::asio::io_service io_service;     // provide core I/O functionality
-    boost::asio::io_service::work work(io_service);     // The work class is a "class to inform the io_service when it has work to do." 
-                                                        // In other words, as long as an io_service has a work object associated with it, it will never run out of stuff to do. 
-                                                        // In this case, "Event processing finishes" never gets printed
-
+/*
+    boost::asio::io_service io_service;                 // provide core I/O functionality
+    boost::asio::io_service::work work(io_service);     // 'work' class is a "class to inform io_service when it has work to do." 
+                                                        // As long as an io_service has a work object associated with it, it will never run out of stuff to do. 
+    
     io_service.run();   // run io_service object's event processing loop
                         // run() blocks until all work has finished and there are no more handlers to be dispatched,
                         //       or until the io_service has been stopped
 
-    std::cout << "Event processing finishes" << std::endl;
+
+
+ */
+#include <boost/asio.hpp>
+#include <iostream>
+
+static void example_run();
+static void example_poll();
+
+static void example_run()
+{
+    boost::asio::io_service io_service;                 
+    //boost::asio::io_service::work work(io_service);           // will block at run() blow if uncommented
+    
+    io_service.run();  
+
+    std::cout << "Event processing finishes" << std::endl;      // never reach here if io_server has work object associated with
+}
+
+static void example_poll()
+{
+    const int NUM_TASKS = 23;
+
+    boost::asio::io_service io_service;
+    boost::asio::io_service::work work(io_service);
+
+    for (int i=0; i<NUM_TASKS; i++) {
+        io_service.poll();
+        std::cout << "Task #" << i << "\n";
+    }
+}
+
+void TEST_boost_asio_api()
+{
+    std::cout << "\nExample: io_service run:\n";
+    example_run();
+
+    std::cout << "\nExample: io_service poll:\n";
+    example_poll();
 }
