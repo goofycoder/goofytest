7bcea38c6d669651e29972b556f3c130845a9c46
xzt <xzt@ubuntu.(none)>
2013-10-31 23:02:16 -0400

    pseudo code for Dijkstra algorithm (shortest path)

---
+++ algo/src/pseudo/shortest_path.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+void shortest_path(GNode* v)
+{
+    std::priority_queue<GNode*> s;
+
+    s.insert(v);
+
+    while (s.empty() == false) {
+        GNode* top = s.top();
+        s.pop();
+
+        // mark top as visited
+        markAsVisited(top);
+        
+        // check termination condition such as destination node is reached
+        check_term_cond();
+
+        vector<GNode*> neighbor;
+        
+        findNeighbor(v, neighbor);
+   
+        for (int i=0; i<v.size(); i++) {
+            s.insert(neighbor[i]);
+        }
+    }
+}
