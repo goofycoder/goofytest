c4b68a9aa03351417873213c3c16016761d7be03
DZT <yanzhou1010@gmail.com>
2014-08-23 08:31:30 -0400

    search rotated array

--- coding/search_rotated_array.cpp
+++ coding/search_rotated_array.cpp
@@ -1,5 +1,21 @@
 /*
+ *  Find the key in the rotated sorted array
+ *
  *  http://leetcode.com/2010/04/searching-element-in-rotated-array.html
+ *  
+ *  Algorithm:
+ *      
+ *      set L to be 0
+ *      set R to be len-1
+ *
+ *      M = L+R/2
+ *
+ *      if(a[L]<=a[M])   // left part is sorted
+ *          if key is in range [L,M], update R to M-1
+ *          if key is not in this range, update R to M+1
+ *      else    // right half is sorted
+ *          if key is in range [M, R], update L to M+1
+ *          else, update R to M-1
  */ 
 #include <iostream>
 using namespace std;
@@ -38,6 +54,7 @@ int search_rotated_array(int a[], int len, int key)
 int main()
 {
     int a[] = {8, 10, 12, 14, 15, 5,7};
+    //int a[] = {1,2,3,4,5};
     int len = sizeof(a)/sizeof(a[0]);
     int key = 12;
 
